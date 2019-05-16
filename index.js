e9c7e588e179717aee40c400bc0e0fd29efe381e
Leslie Thompson <lcrthompson@gmail.com>
2019-05-16 10:23:52 -0700

    initial

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
+const port = process.env.PORT || 4000;
+//Heroku will be looking for an environment with PORT
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
